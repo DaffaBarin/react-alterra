533655c1e0ed7f92a6f8e53c7e72dd2148b4c556
DaffaBarin <daffabarin@gmail.com>
2021-11-15 11:16:26 +0700

    review-task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, ContactUs, ReviewMessage, News, NotFound } from './Pages'
+import { Provider } from 'react-redux';
+import store from './Redux/store';
 
-import Navbar from './Navbar';
-import Body from './Body';
-import ContactUs from './ContactUs';
 function App() {
   return (
-    <div className="App">
-      {/* <Navbar/>
-      <Body /> */}
-      <ContactUs/>
-    </div>
+    <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path="/contact_us" exact element={<ContactUs />} />
+            <Route path="/review_message" element={<ReviewMessage />} />
+            <Route path="/news" element={<News />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+    </Provider>
   );
 }
 
