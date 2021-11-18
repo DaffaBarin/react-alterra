fbc84ee8197f0b540454258186b11c64434c464a
DaffaBarin <daffabarin@gmail.com>
2021-11-18 12:29:06 +0700

    graphql-task-2

--- src/component/ListPassenger.js
+++ src/component/ListPassenger.js
@@ -1,14 +1,16 @@
 import ListItem from './ListItem';
-import { gql, useQuery,useLazyQuery } from '@apollo/client';
+import { gql, useQuery,useLazyQuery, useMutation } from '@apollo/client';
 import { useState } from 'react';
 
 const ListPassenger = (props) => {
     const [viewId, setId] = useState('');
     const [userData, setUserData] = useState([]);
+    const [isEdit, setEdit] = useState(null);
+    const [editingText, setEditingText] = useState('');
 
-    const getDataListQuery = gql`
-        query MyQuery($id: Int!) {
-            anggota(where: { id: { _eq: $id } }) {
+    const getDataList = gql`
+        query MyQuery {
+            anggota{
                 id
                 jenisKelamin
                 nama
@@ -16,16 +18,37 @@ const ListPassenger = (props) => {
             }
         }
     `;
+        
+    const editDataList = gql`
+        mutation MyMutation($id: Int!, $nama: String!) {
+            update_anggota(where: { id: { _eq: $id } }, _set: { nama: $nama }) {
+                returning {
+                    id
+                    jenisKelamin
+                    nama
+                    umur
+                }
+            }
+        }
+    `;
 
-    const [getData, { data, loading, error }] = useLazyQuery(getDataListQuery);
-    console.log(data);
+    const deleteDataList = gql`
+        mutation MyMutation($id: Int!) {
+            delete_anggota_by_pk(id: $id) {
+                nama
+            }
+        }
+    `;
+    const { data, loading, error, refetch } = useQuery(getDataList);
+    const [editData, { data: dataMutation, loading: loadingMutation, error: errorMutation }] = useMutation(
+        editDataList,
+        { refetchQueries: [getDataList] }
+    );
+
+    const [deleteData, { loading: dataDeleteLoading }] = useMutation(deleteDataList, { refetchQueries: [getDataList] });
+
     const onClickData = () => {
-        getData({
-            variables: {
-                id: viewId,
-            },
-        });
-        setUserData(data?.anggota);
+        console.log('clicked');
     };
 
     const onInputId = (e) => {
@@ -33,10 +56,26 @@ const ListPassenger = (props) => {
             setId(e.target.value);
         }
     };
+
+    const hapusPengunjung = (id) => {
+        deleteData({ variables: { id: id } });
+    };
+
+    const setEditId = (id) => {
+        if (id) {
+            setEdit(id);
+        }
+    };
+    const handleChangeEdit = (e) => {
+        setEditingText(e.target.value);
+    };
+    const submitEdit = (id) => {
+        editData({ variables: { id: id, nama: editingText } });
+        setEdit(null);
+        refetch();
+    };
     return (
         <div>
-            <input value={viewId} onChange={onInputId} />
-            <button onClick={onClickData}>Get Data</button>
             <table cellPadding="5px" cellSpacing="0" style={{ margin: 'auto' }}>
                 <thead bgcolor="red">
                     <td>Nama</td>
@@ -45,7 +84,19 @@ const ListPassenger = (props) => {
                     <td bgcolor="white" className="removeBorder"></td>
                 </thead>
                 {data?.anggota.map((item) => (
-                    <ListItem key={item.id} data={item} loading={loading} hapusPengunjung={props.hapusPengunjung} />
+                    <ListItem
+                    key={item.id}
+                    data={item}
+                    loading={loading}
+                    hapusPengunjung={hapusPengunjung}
+                    setEditId={setEditId}
+                    isEdit={isEdit}
+                    setEditingText={setEditingText}
+                    editingText={editingText}
+                    handleChangeEdit={handleChangeEdit}
+                    submitEdit={submitEdit}
+                    loadingMutation={loadingMutation}
+                />
                 ))}
             </table>
         </div>
